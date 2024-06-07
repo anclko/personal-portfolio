7c27b322eb5b908f2af27c14409058cf50790ad8
anclko <ackoch@hotmail.com>
2024-06-07 13:07:34 +0200

    implement stair effect in accent color

--- client/components/StairTransition.jsx
+++ client/components/StairTransition.jsx
@@ -3,6 +3,8 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
+import Stairs from "./Stairs";
+
 const StairTransition = () => {
     const pathname = usePathname();
 
@@ -12,10 +14,10 @@ const StairTransition = () => {
           <div key={pathname}>
             <div className="h-screen w-screen fixed top-0 left-0 right-0
             pointer-events-none z-40 flex">
-                Stairs
+                <Stairs />
             </div>
           </div>
-        </AnimatePresence>;
+        </AnimatePresence>
       </>
     );
 };
