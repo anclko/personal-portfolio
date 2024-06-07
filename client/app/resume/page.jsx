af1900623bc2ffd54516cc638ade8de4560a464f
anclko <ackoch@hotmail.com>
2024-06-07 11:03:42 +0200

    set up page for navbar

---
+++ client/app/resume/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>Resume page</div>
+    )
+}
+
+export default page
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -34,7 +34,8 @@ const Nav = () => {
                         href={link.path} 
                         key={index} 
                         className={`${
-                            link.path === pathname && "text-accent border-b-2 border-accent"}`}
+                            link.path === pathname && "text-accent border-b-2 border-accent"
+                        } capitalize font-medium hover:text-accent transition-all`}
                     >
                         {link.name}
                     </Link>
