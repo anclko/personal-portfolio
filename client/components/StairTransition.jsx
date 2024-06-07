754bf720c474937ec4e64bd412c38ec0edcb67ce
anclko <ackoch@hotmail.com>
2024-06-07 13:12:31 +0200

    add duration and delay to stair transition

--- client/components/StairTransition.jsx
+++ client/components/StairTransition.jsx
@@ -16,6 +16,10 @@ const StairTransition = () => {
             pointer-events-none z-40 flex">
                 <Stairs />
             </div>
+
+            <motion.div className="h-screen w-screen fixed bg-primary top-0
+            pointer-events-none" initial={{opacity: 1}} animate={{opacity: 0, transition:{delay: 0.5, duration: 0.5, ease: "easeInOut"}
+            }} />
           </div>
         </AnimatePresence>
       </>
