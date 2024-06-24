31568e2c0e63596ba82c6547b65fb8d84eb17a10
anclko <ackoch@hotmail.com>
2024-06-24 12:32:05 +0200

    add text to page, download CV button + mobile view

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -7,11 +7,33 @@ const Home = () => {
       <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
         {/* text */}
         <div className="text-center xl:text-left">
-          <span>Software Developer</span>
-          <h1 className="h1">
+          <span className="text-xl">Software Developer</span>
+          
+          <h1 className="h1 mb-6">
             Hello I'm <br />
-            <span>Anne-Claire Koch</span>
+            <span className="text-accent">Anne-Claire Koch</span>
           </h1>
+
+          <p className="max-w-[500px] mb-9 text-white/80">
+            I am a third-year student with a strong passion for full-stack development and hands-on experience in various web technologies.
+          </p>
+          
+          <div className="flex flex-col xl:flex-row items-center gap-8">
+            {/* CV Downloads */}
+            <Button 
+            variant="outline" 
+            size="lg" 
+            className="uppercase flex items-center gap-2"
+            >
+              <span>Download CV</span>
+              <FiDownload className="text-xl" />
+            </Button>
+
+            {/* Socials */}
+            <div className="mb-8 xl:mb-0">Socials</div>
+
+          </div>
+
         </div>
 
         {/* profile pic */}
