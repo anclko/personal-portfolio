918bb31bec38d5906cc231e2d4ad1b41b5ee70c2
anclko <ackoch@hotmail.com>
2024-06-30 10:10:01 +0200

    implement "about me" section for resume page

--- client/app/resume/page.jsx
+++ client/app/resume/page.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import { 
+    FaHtml5, 
+    FaCss3, 
+    FaJs, 
+    FaReact, 
+    FaFigma, 
+    FaNodeJs 
+} from "react-icons/fa"
+
+import { 
+    SiTailwindcss,
+    SiNextdotjs
+} from "react-icons/si"
+
+
+//about me
+const about = {
+    title: 'About Me',
+    description: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien fermentum tincidunt nec nec libero",
+    info: [
+        {
+            fieldName: "Name",
+            fieldValue: "Anne-Claire Koch"
+        },
+        {
+            fieldName: "Phone",
+            fieldValue: "(+46) 793 566 601)"
+        },
+        {
+            fieldName: "Experience",
+            fieldValue: "2 Years"
+        },
+        {
+            fieldName: "Location",
+            fieldValue: "Sweden"
+        },
+        {
+            fieldName: "Nationality",
+            fieldValue: "French"
+        },
+        {
+            fieldName: "Email",
+            fieldValue: "ackoch@hotmail.com"
+        },
+        {
+            fieldName: "Job Status",
+            fieldValue: "Available to work now"
+        },
+        {
+            fieldName: "Languages",
+            fieldValue: "English, French, German, Swedish"
+        },
+    ]
+};
+
+const Resume = () => {
     return (
         <div>Resume page</div>
     )
 }
 
-export default page
+export default Resume
