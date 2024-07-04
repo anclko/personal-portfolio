07de34158fffc72973d55b7a3accd06af2a7f4b1
anclko <ackoch@hotmail.com>
2024-07-04 10:44:15 +0200

    implement skills section with icons

--- client/app/resume/page.jsx
+++ client/app/resume/page.jsx
@@ -283,7 +283,31 @@ const Resume = () => {
 
                     {/* skills */}
                     <TabsContent value="skills" className="w-full">
-                        skills
+                        <div className="flex flex-col gap-[30px]">
+                          <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                            <h3 className="text-4xl font-bold">{skill.title}</h3>
+                            <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                              {skill.description}
+                            </p>
+                            <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                              {skill.skillList.map((skill, index) => {
+                                return(
+                                  <li key={index}>
+                                    <TooltipProvider>
+                                      <Tooltip>
+                                        <TooltipTrigger>
+                                            <div>
+                                              {skill.icon}
+                                            </div>
+                                        </TooltipTrigger>
+                                      </Tooltip>
+                                    </TooltipProvider>
+                                  </li>
+                                );
+                              })}
+                            </ul>
+                          </div>  
+                        </div>
                     </TabsContent>
 
                     {/* about us */}
