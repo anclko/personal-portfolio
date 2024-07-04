e24ae6813655937ab6f4bf0f1a3d6b41371f2790
anclko <ackoch@hotmail.com>
2024-07-04 09:59:30 +0200

    customize experience scroll area

--- client/app/resume/page.jsx
+++ client/app/resume/page.jsx
@@ -23,6 +23,8 @@ import {
     TooltipTrigger,
 } from "@/components/ui/scroll-area"
 
+import { ScrollArea } from "@/components/ui/scroll-area"
+
 import { motion } from "framer-motion"
 
 //about me
@@ -73,6 +75,26 @@ const experience = {
     description:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien fermentum tincidunt nec nec libero",
     items: [
+        {
+            company: "Company",
+            position: "Position",
+            duration: "Duration",
+        },
+        {
+            company: "Company",
+            position: "Position",
+            duration: "Duration",
+        },
+        {
+            company: "Company",
+            position: "Position",
+            duration: "Duration",
+        },
+        {
+            company: "Company",
+            position: "Position",
+            duration: "Duration",
+        },
         {
             company: "Company",
             position: "Position",
@@ -199,13 +221,30 @@ const Resume = () => {
                     {/* experience */}
                     <TabsContent value="experience" className="w-full">
                         <div className="flex flex-col gap-[30px] text-center xl:text-left">
-                            <h3 className="text-4xl font-bold">
-                                {experience.title}
-                            </h3>
-
+                            <h3 className="text-4xl font-bold">{experience.title}</h3>
                             <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
                             {experience.description}
                             </p>
+
+                            <ScrollArea className="h-[400px]">
+                                <ul className=" grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                                  {experience.items.map((item, index) => {
+                                    return(
+                                      <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                                        <span className="text-accent">{item.duration}</span>
+                                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                                            {item.position}
+                                        </h3>
+                                        <div className="flex items-center gap-3">
+                                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                                            <p className="text-white/60">{item.company}</p>
+                                        </div>
+                                      </li>
+
+                                    );
+                                })}
+                                </ul>
+                            </ScrollArea>
 
                         </div>
                     </TabsContent>
