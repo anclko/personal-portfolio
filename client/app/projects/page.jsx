973fee3fd7a2890f35708e176df977f05c4317ca
anclko <ackoch@hotmail.com>
2024-07-09 16:31:18 +0200

    implement slider buttons with styling

--- client/app/projects/page.jsx
+++ client/app/projects/page.jsx
@@ -17,6 +17,7 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
+import WorkSliderBtns from "@/components/ui/WorkSliderBtns";
 
 const projects = [
     {
@@ -31,20 +32,20 @@ const projects = [
             { name: "MongoDB"}, { name: "React"}, { name: "Tailwind CSS"}, { name: "Jira"}
         ],
         image: '/assets/work/Project1.jpg',
-        live: '',
-        github: '',
+        live: 'https://www.funbyte.eu/',
+        github: 'https://github.com/ErickOlibo/FunByte',
     },
     {
         num: "02",
         category: "Fullstack",
-        title: "Recipe Book",
+        title: "Online Resume/Portfolio",
         description: 
           "loren ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         stack: 
         [
             { name: "Html 5" }, { name: "Css 3" }, { name: "Javascript"}, { name: "Node.js"}, { name: "Express.js"},
         ],
-        image: '',
+        image: '/assets/work/Project2.jpg',
         live: '',
         github: '',
     },
@@ -92,7 +93,7 @@ const Work = () => {
     return (
       <motion.section
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        animate={{ opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"} }}
         className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
       >
         <div className="container mx-auto">
@@ -183,7 +184,7 @@ const Work = () => {
                   return<SwiperSlide key={index} className="w-full">
                     <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
                       {/* overflow */}
-                      <div></div>
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       {/* image */}
                       <div className="relative w-full h-full">
                         <Image 
@@ -196,6 +197,9 @@ const Work = () => {
                     </div>
                   </SwiperSlide>
                 })}
+                {/* buttons for slider */}
+                <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-white hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"/>
               </Swiper>
             </div>
           </div>
