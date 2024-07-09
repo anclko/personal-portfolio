146c6390635ac1b513adc645bb6a8701ea75f4c5
anclko <ackoch@hotmail.com>
2024-07-09 13:01:58 +0200

    fix stack elements by adding them in cols

--- client/app/projects/page.jsx
+++ client/app/projects/page.jsx
@@ -28,7 +28,7 @@ const projects = [
         stack: 
         [
             { name: "HTML" }, { name: "Javascript"}, { name: "Node.js" }, { name: "Express.js" },
-            { name: "MongoDB"}, { name: "React"}, { name: "Tailwind CSS"}
+            { name: "MongoDB"}, { name: "React"}, { name: "Tailwind CSS"}, { name: "Jira"}
         ],
         image: '/assets/work/Project1.jpg',
         live: '',
@@ -110,12 +110,11 @@ const Work = () => {
                 </p>
 
                 {/*stack*/}
-                <ul className="flex gap-4">
+                <ul className="grid grid-cols-2 gap-2">
                   {project.stack.map((item, index) => {
                     return (
-                      <li key={index} className="text-xl text-accent">
+                      <li key={index} className="text-md text-accent">
                         {item.name}
-                        {index !== project.stack.length - 1 && ","}
                       </li>
                     );
                   })}
