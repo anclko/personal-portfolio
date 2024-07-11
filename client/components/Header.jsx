4ccd67dbec64ac3526ff7cd6960d415c8e2ea8b9
anclko <ackoch@hotmail.com>
2024-07-11 11:57:02 +0200

    add email to "hire me" button on navbar

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Button } from "./ui/button"
+import { Button } from "./ui/button";
 
 import Navbar from "./Navbar";
 import MobileNavbar from "./MobileNavbar";
@@ -7,32 +7,29 @@ import MobileNavbar from "./MobileNavbar";
 const Header = () => {
     return (
         <header className="py-8 xl:py-12 text-white"> 
-
-           <div className="container mx-auto flex justify-between items-center">
-              {/* LOGO */}
-              <Link href="/">
-                <h1 className="text-4xl font-semibold">
-                Anne-Claire<span className="text-accent">.</span>
-                </h1>
-              </Link>
-
-
-              {/* Desktop navbar*/}
-              <div className="hidden xl:flex items-center gap-8">
-                <Navbar /> 
-                <Link href="/contact">
-                  <Button>Hire Me</Button>
+            <div className="container mx-auto flex justify-between items-center">
+                {/* LOGO */}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Anne-Claire<span className="text-accent">.</span>
+                    </h1>
                 </Link>
-              </div>
 
-              {/* Mobile navbar */}
-              <div className="xl:hidden">
-                <MobileNavbar />
-              </div>
-              
-           </div> 
+                {/* Desktop navbar*/}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Navbar />
+                    <a href="mailto:ackoch@hotmail.com">
+                        <Button>Hire Me</Button>
+                    </a>
+                </div>
+
+                {/* Mobile navbar */}
+                <div className="xl:hidden">
+                    <MobileNavbar />
+                </div>
+            </div> 
         </header>
     );
 };
 
-export default Header
+export default Header;
