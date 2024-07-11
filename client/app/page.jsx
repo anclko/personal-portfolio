9ebafe558763111effbe3e533583fc6e1858bf76
anclko <ackoch@hotmail.com>
2024-07-11 12:41:10 +0200

    get download cv to work with cv path

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -6,53 +6,55 @@ import Photo from '@/components/Photo';
 import Stats from '@/components/Stats';
 
 const Home = () => {
-  return <section className="h-full">
-    <div className="container mx-auto h-full">
-      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-        {/* text */}
-        <div className="text-center xl:text-left order-2 xl:order-none">
-          <span className="text-xl">Software Developer</span>
-          
-          <h1 className="h1 mb-6">
-            Hello I'm <br />
-            <span className="text-accent">Anne-Claire Koch</span>
-          </h1>
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Software Developer</span>
 
-          <p className="max-w-[500px] mb-9 text-white/80">
-            I am a third-year student with a strong passion for full-stack development and hands-on experience in various web technologies.
-          </p>
-          
-          <div className="flex flex-col xl:flex-row items-center gap-8">
-            {/* CV Downloads */}
-            <Button 
-            variant="outline" 
-            size="lg" 
-            className="uppercase flex items-center gap-2"
-            >
-              <span>Download CV</span>
-              <FiDownload className="text-xl" />
-            </Button>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />
+              <span className="text-accent">Anne-Claire Koch</span>
+            </h1>
 
-            {/* Socials */}
-            <div className="mb-8 xl:mb-0">
-              <Social 
-                containerStyles="flex gap-6" 
-                iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-white text-base 
-                hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I am a third-year student with a strong passion for full-stack development and hands-on experience in various web technologies.
+            </p>
+
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              {/* CV Downloads */}
+              <a href="/assets/resume/resume.pdf" download>
+                <Button 
+                  variant="outline" 
+                  size="lg" 
+                  className="uppercase flex items-center gap-2 text-white"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
+
+              {/* Socials */}
+              <div className="mb-8 xl:mb-0">
+                <Social 
+                  containerStyles="flex gap-6" 
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-white text-base 
+                  hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+              </div>
             </div>
-
           </div>
 
-        </div>
-
-        {/* profile pic */}
-        <div className="order-1 xl:order-none mb-8 xl:mb-0">
-          <Photo />
-        </div>
-      </div> 
-    </div>
-    <Stats />
-  </section>
+          {/* profile pic */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div> 
+      </div>
+      <Stats />
+    </section>
+  );
 }
 
 export default Home;
